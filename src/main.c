ca7f29241b4d955c8cdd7ff220d7578cf673e76f
Julien BALLANGER <jballang@e2r11p22.42.fr>
2018-05-03 16:49:18 +0200

    Update main tests

--- src/main.c
+++ src/main.c
@@ -6,37 +6,42 @@
 /*   By: jballang <jballang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 10:35:11 by jballang          #+#    #+#             */
-/*   Updated: 2018/04/27 10:16:50 by jballang         ###   ########.fr       */
+/*   Updated: 2018/05/03 16:47:22 by jballang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
-int	main(int ac, char **av)
+int	main(void)
 {
-	void	*ptr;
-	/*void	*ptr2;
+	//void	*ptr;
+	//void	*ptr2;
 	void	*ptr3;
-	void	*ptr4;*/
+	void	*ptr4;
 
-	ac = 777;
-	av = NULL;
-	int i = 0;
-	while (i < 196) {
+	//int i = 0;
+	/*while (i < 196) {
 		ptr = fmalloc(128);
 		i += 1;
 	}
 	ptr = fmalloc(57);
-	ptr = fmalloc(30);
-	/*ptr2 = fmalloc(222);
+	ptr = fmalloc(77);
+	ptr2 = fmalloc(222);
 	ptr3 = (char*)fmalloc(6);
-	ptr4 = (char*)fmalloc(15);*/
-	show_pages();
-	/*ft_memcpy(ptr3, "super\0", 6);
+	ptr4 = (char*)fmalloc(15);
+	ptr = malloc(33);
+	ptr3 = malloc(9000);
+	ptr2 = malloc(777);*/
+	ptr4 = fmalloc(9000);
 	ft_memcpy(ptr4, "incroyable!__!\0", 15);
-	ft_putendl(ptr3);
 	ft_putendl(ptr4);
-	ft_putendl(ptr3 + (sizeof(t_header) + 6));*/
+	show_pages();
+	ptr3 = fmalloc(4);
+	ptr4 = frealloc(ptr4, 444);
+	show_pages();
+	//ft_putendl(ptr4);
+	//ft_putendl(ptr3 + (sizeof(t_header) + 6));
+	return (0);
 }
 
 // TINY_MAX 32768
