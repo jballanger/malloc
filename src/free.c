bd1ce4ae3b87929dd89bc0c2e954038c47492355
Julien BALLANGER <jballang@e2r11p22.42.fr>
2018-05-03 16:08:07 +0200

    Remove tmp variables, add conditions

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: jballang <jballang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 10:32:50 by jballang          #+#    #+#             */
-/*   Updated: 2018/04/27 14:33:37 by jballang         ###   ########.fr       */
+/*   Updated: 2018/05/03 12:15:55 by jballang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,39 @@
 void	ffree(void *ptr)
 {
 	t_page		*page;
-	t_page		*tmp_page;
 	t_header	*header;
-	t_header	*tmp_header;
 
 	if (!ptr)
 		return ;
 	header = (ptr - sizeof(t_header));
-	tmp_header = header;
-	while (tmp_header && tmp_header->prev)
-		tmp_header = tmp_header->prev;
-	page = (void*)(tmp_header - 1);
-	page->available += header->size;
-	header->free = 1;
+	while (header && header->prev)
+		header = header->prev;
+	page = (void*)(header - 1);
+	header = (ptr - sizeof(t_header));
 	if (page->type == 3)
 	{
-		tmp_page = g_mem.pages;
-		while (tmp_page && tmp_page != page)
-			tmp_page = tmp_page->next;
-		if (tmp_page && tmp_page->prev)
-			tmp_page->prev->next = tmp_page->next;
-		if (tmp_page && tmp_page->next)
-			tmp_page->next->prev = tmp_page->prev;
+		if (!page->next && !page->prev)
+			g_mem.pages = NULL;
+		else
+		{
+			/*tmp_page = g_mem.pages;
+			while (tmp_page && tmp_page != page)
+				tmp_page = tmp_page->next;
+			if (tmp_page && tmp_page->prev)
+				tmp_page->prev->next = tmp_page->next;
+			if (tmp_page && tmp_page->next)
+				tmp_page->next->prev = tmp_page->prev;*/
+			if (page->prev)
+				page->prev->next = page->next;
+			if (page->next)
+				page->next->prev = page->prev;
+		}
 		munmap(page, (sizeof(t_header) + sizeof(t_page) + header->size));
+		page = NULL;
+	}
+	else
+	{
+		page->available += header->size;
+		header->free = 1;
 	}
 }
