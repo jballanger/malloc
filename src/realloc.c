b9620eeb99e918660f650f24c820ed78391cf89e
Julien BALLANGER <jballang@e2r11p22.42.fr>
2018-05-04 15:28:29 +0200

    Add log and rename functions

--- src/realloc.c
+++ src/realloc.c
@@ -6,7 +6,7 @@
 /*   By: jballang <jballang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/03 11:04:39 by jballang          #+#    #+#             */
-/*   Updated: 2018/05/03 12:00:17 by jballang         ###   ########.fr       */
+/*   Updated: 2018/05/04 14:07:33 by jballang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_realloc_tiny_small(void *ptr, size_t size, t_header *header)
 	void	*new_ptr;
 	size_t	new_size;
 
-	new_ptr = fmalloc(size);
+	new_ptr = malloc(size);
 	new_size = (header->size > size) ? size : header->size;
 	ft_memcpy(new_ptr, ptr, new_size);
 	header->free = 1;
@@ -29,10 +29,10 @@ void	*ft_realloc_large(void *ptr, size_t size, t_header *header)
 	void	*new_ptr;
 	size_t	new_size;
 
-	new_ptr = fmalloc(size);
+	new_ptr = malloc(size);
 	new_size = (header->size > size) ? size : header->size;
 	ft_memcpy(new_ptr, ptr, new_size);
-	ffree(ptr);
+	free(ptr);
 	return (new_ptr);
 }
 
@@ -53,13 +53,16 @@ void	*ft_realloc(void *ptr, size_t size)
 	return (NULL);
 }
 
-void	*frealloc(void *ptr, size_t size)
+void	*realloc(void *ptr, size_t size)
 {
+	ft_putstr("Call to realloc for ");
+	ft_putnbr(size);
+	ft_putchar('\n');
 	if (!ptr)
-		return fmalloc(size);
+		return malloc(size);
 	if (size == 0)
 	{
-		ffree(ptr);
+		free(ptr);
 		return (NULL);
 	}
 	return (ft_realloc(ptr, size));
