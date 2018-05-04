35d0c7b6389cc2127c6172abadb8ff32fa5b96df
Julien BALLANGER <jballang@e2r11p22.42.fr>
2018-05-04 15:29:46 +0200

    Add own_ptr function

--- src/free.c
+++ src/free.c
@@ -6,18 +6,67 @@
 /*   By: jballang <jballang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 10:32:50 by jballang          #+#    #+#             */
-/*   Updated: 2018/05/03 16:45:27 by jballang         ###   ########.fr       */
+/*   Updated: 2018/05/04 15:29:23 by jballang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
-void	ffree(void *ptr)
+t_page		*search_ptr_page(void *ptr, t_page *pages)
+{
+	size_t	page_max_addr;
+
+	while (pages)
+	{
+		if (pages->type == 1)
+			page_max_addr = (size_t)(pages + (TINY_MAX - 1));
+		else if (pages->type == 2)
+			page_max_addr = (size_t)(pages + (SMALL_MAX - 1));
+		else if (pages->type == 3)
+			page_max_addr = (size_t)(pages + (sizeof(t_page) + sizeof(t_header) + pages->available - 1));
+		if ((size_t)ptr > (size_t)pages && (size_t)ptr < page_max_addr)
+			return (pages);
+		pages = pages->next;
+	}
+	return (NULL);
+}
+
+t_header	*search_ptr_header(void *ptr, t_page *page)
+{
+	t_header	*header;
+
+	header = page->blocks;
+	while (header)
+	{
+		if ((size_t)ptr == (size_t)header->address)
+			return (header);
+		header = header->next;
+	}
+	return (NULL);
+}
+
+int			own_ptr(void *ptr)
+{
+	t_page		*pages;
+	t_page		*ptr_page;
+	t_header	*header;
+
+	pages = g_mem.pages;
+	ptr_page = search_ptr_page(ptr, pages);
+	if (ptr_page)
+	{
+		header = search_ptr_header(ptr, ptr_page);
+		return (header ? 1 : 0);
+	}
+	return (0);
+}
+
+void		free(void *ptr)
 {
 	t_page		*page;
 	t_header	*header;
 
-	if (!ptr)
+	if (!ptr || !own_ptr(ptr))
 		return ;
 	header = (ptr - sizeof(t_header));
 	while (header && header->prev)
@@ -26,6 +75,7 @@ void	ffree(void *ptr)
 	header = (ptr - sizeof(t_header));
 	if (page->type == 3)
 	{
+		ft_putendl("FREE LARGE PAGE");
 		if (page->prev)
 			page->prev->next = page->next;
 		if (page->next)
